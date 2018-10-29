d0b6bfbd5eed0892f844733511d99b71c1fe2030
Guilherme Teixeira <guilherme@vert-capital.com>
2018-10-29 18:18:05 -0300

    Finaliza a aula 5 (Bancos de dados)

--- empresax/core/models.py
+++ empresax/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    nome = models.CharField(max_length=60)
+    senha = models.CharField(max_length=16)
+
+    def __str__(self):
+        return self.nome
