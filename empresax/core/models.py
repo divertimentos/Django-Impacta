79f70b9f87842547644fd3d62917d967a767514f
Guilherme Teixeira <guilherme@vert-capital.com>
2018-10-30 12:33:22 -0300

    Finaliza a aula 6 (parte 3): "Validando o cadastro de usuário".

--- empresax/core/models.py
+++ empresax/core/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Usuario(models.Model):
     nome = models.CharField(max_length=60)
     senha = models.CharField(max_length=16)
--- empresax/core/views.py
+++ empresax/core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Usuario
 from django.http import HttpResponse
 
 
@@ -7,8 +8,25 @@ def index(request):
 
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == 'GET':
+        return render(request, 'cadastro.html')
+    elif request.method == 'POST':
+        if request.POST.get('nome') and request.POST.get('senha'):
+            user = Usuario()
+            user.nome = request.POST['nome']
+            user.senha = request.POST['senha']
+            user.save()
+            return render(request, 'cadastro.html')
+        else:
+            return render(request, 'cadastro.html')
+    
 
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        nome = request.POST('nome')
+        senha = request.POST('senha')
+
+        return render(request, 'login.html')
+    else:
+        return HttpResponse("Erro ao carregar!")
